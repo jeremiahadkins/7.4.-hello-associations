f9b025632e7af5f6ef54e02948fb8e12cd81e9d5
Jeremiah Adkins <hi@jeremiahadkins.com>
2017-08-31 09:54:43 -0400

    inital setup of sequelize

---
+++ app.js
@@ -0,0 +1,16 @@
+const models = require('./models');
+
+// models.User.create({
+//   username: "Young Thugger",
+//   password: "gucci"
+// });
+
+// models.User.create({
+//   username: "Freeze",
+//   password: "icey"
+// });
+
+// models.User.create({
+//   username: "Ted",
+//   password: "bed"
+// });
