88d15e6791e8a138f68eb79ac188ee634d0a38cc
Jeremiah Adkins <hi@jeremiahadkins.com>
2017-08-31 11:33:26 -0400

    changed index to find dog and change owner

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const models = require('./models');
 
-
-models.User.findOne({
-  where: {username: 'Ted'}
-}).then((user) => {
-  let newDog = models.Dog.build({
-    name: 'Champ',
-    isWearingHat: true,
-    isAggressive: false
+models.Dog.findOne({
+  where: {name: 'Champ'}
+}).then((dog) => {
+  models.User.findOne({
+    where: {username: 'Young Thugger'}
+  }).then((user) => {
+    dog.userId = user.id;
+    dog.save();
   });
-  // if user is found, create dog with user
-  // if user is not found, create dog without user
-  if(user !== null) {
-    newDog.userId = user.id;
-  }
-  newDog.save();
 });
+
+// models.User.findOne({
+//   where: {username: 'Ted'}
+// }).then((user) => {
+//   let newDog = models.Dog.build({
+//     name: 'Champ',
+//     isWearingHat: true,
+//     isAggressive: false
+//   });
+//   // if user is found, create dog with user
+//   // if user is not found, create dog without user
+//   if(user !== null) {
+//     newDog.userId = user.id;
+//   }
+//   newDog.save();
+// });
 
